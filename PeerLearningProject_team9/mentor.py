99ede131ab1cd435fd85cd02b36b1c72b57c1452
Brenda-Maina <bmaina@alustudent.com>
2025-03-29 14:23:24 +0000

    submission: fixed mentor profile function for Python 2 compatibility

---
+++ PeerLearningProject_team9/mentor.py
@@ -0,0 +1,8 @@
+# Task 5: Mentor Profiles
+
+# Predefined mentor profiles
+mentors = {
+    "Technology": "Darcy Teta, Expert in AI and Machine Learning",
+    "Agriculture": "Leila Cyuzuzo, Sustainable Farming Expert",
+    "Art": "Hervé Mugisha, Visual Arts Specialist"
+}
